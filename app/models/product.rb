f47251172c7bf2dfda9522e49cdacd81fb7eba8d
Dmitry Stepanenko <dms@New-Mac-Pro.local>
2014-04-16 12:36:02 +0300

    Validations and first tests.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
+  validates :title, uniqueness: true
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'URL must be GIF, JPG or PNG format.'
+  }
   attr_accessible :description, :image_url, :price, :title
 end
