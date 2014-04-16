38656436884c470aab2b1a86ded095d99152773c
Dmitry Stepanenko <dms@New-Mac-Pro.local>
2014-04-16 11:08:21 +0300

    Deport scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileProj::Application.config.secret_token = 'ef139c02f26bea67fcb3b660820e2cf89ce96808948cfc31bf109d1b1cb0eb2aa65f9bb03179f8401cecc1024aac3ac85d15911682f068cc7da37875002847c3'
